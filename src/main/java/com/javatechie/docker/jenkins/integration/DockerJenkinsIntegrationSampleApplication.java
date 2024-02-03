1f49a69e3675b6eb72af8f1c428d151dca89626c
Bhimashi <bhimashi@greentinsolutions.com>
2024-02-03 12:33:02 +0530

    first commit

---
+++ src/main/java/com/javatechie/docker/jenkins/integration/DockerJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.docker.jenkins.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
